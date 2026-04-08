b065c998588c8967f5bc9d8f7cda300a73a55f7e
Your Name <you@example.com>
2026-04-08 13:19:37 +0200

    Creación de Interface EnpleadosEnProyectoDao

---
+++ src/modelo/dao/EmpleadosEnProyectoDao.java
@@ -0,0 +1,27 @@
+package modelo.dao;
+
+import java.util.List;
+
+import modelo.entities.ProyectoConEmpleados;
+
+public interface EmpleadosEnProyectoDao {
+	
+	void crearProyectoConEmpleados(ProyectoConEmpleados proyectoConEmpleados);
+	
+	ProyectoConEmpleados buscarProyectoConEmpleados(int numeroOrden);
+	
+	void actualizarProyectoConEmpleados(ProyectoConEmpleados proyectoConEmpleados);
+	
+	boolean eliminarProyectoConEmpleados(int numeroOrden);
+	
+	List<ProyectoConEmpleados> findAll();
+	
+	List<Empleado> empleadosByProyecto(String codigoProyecto);
+	
+	int asignarEmpleadosAProyecto(List<ProyectoConEmpleados>empleados);
+	
+	int horasAsignadasAProyecto(String codigoProyecto);
+	
+	double costeActualDeEmpleadosEnProyecto(String codigoProyecto);
+
+}
